3970f2f04f67e1f69a67cd2d0f0d483e96035344
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-05-14 14:09:42 +0200

    added animation svg

--- src/components/heartNeon.tsx
+++ src/components/heartNeon.tsx
@@ -40,40 +40,40 @@ const HeartNeon: React.FC<HeartNeonProps> = ({ onActivated, onIgnite }) => {
             </feMerge>
           </filter>
 
-          {/* Curve path along which the text "PIĘKNY SYF" will bend */}
-          <path id="text-curve" d="M 150,520 Q 400,470 650,520" />
+          {/* Curve path along which the text "PIĘKNY SYF" will bend. 
+              Lifted higher and smoothed to match the original logo */}
+          <path id="text-curve" d="M 170,540 Q 400,450 630,540" />
         </defs>
 
-        {/* Main heart outline (classic, smooth shape) */}
+        {/* Main heart outline - refined to be smoother and better proportioned */}
         <path
           className="heart-path"
-          d="M 400, 300
-             C 400, 300 320, 180 200, 180
-             C 80, 180 80, 380 200, 480
-             L 400, 680
-             L 600, 480
-             C 720, 380 720, 180 600, 180
-             C 480, 180 400, 300 400, 300 Z"
+          d="M 400,680 
+             C 400,680 150,450 150,280 
+             C 150,150 350,150 400,300 
+             C 450,150 650,150 650,280 
+             C 650,450 400,680 400,680 Z"
         />
 
-        {/* Top flames (sharp, organic) */}
+        {/* Top flames (unified crown, organic and sweeping upward) */}
         <path
           className="flame"
-          d="M 240,210
-             Q 210,120 250,40
-             Q 280,100 290,140
-             Q 310,70 340,30
-             Q 360,110 370,140
-             Q 390,40 430,10
-             Q 440,100 450,150
-             Q 480,80 510,50
-             Q 510,130 530,160
-             Q 560,100 580,80
-             Q 550,160 560,210"
+          d="M 220,250 
+             C 200,120 250,50 250,50 
+             C 260,110 270,160 290,160 
+             C 320,70 340,30 340,30 
+             C 350,110 360,140 380,140 
+             C 410,40 450,10 450,10 
+             C 450,110 460,160 480,160 
+             C 520,60 550,50 550,50 
+             C 530,130 540,170 560,170 
+             C 590,90 620,120 620,120 
+             C 580,190 600,240 600,240"
         />
+        
         {/* Inner details for upper flames for depth */}
-        <path className="flame" d="M 330,160 Q 340,90 370,60 Q 380,120 400,150" />
-        <path className="flame" d="M 420,160 Q 440,80 480,60 Q 470,130 490,170" />
+        <path className="flame" d="M 310,140 Q 330,60 350,100" />
+        <path className="flame" d="M 420,140 Q 440,50 470,90" />
 
         {/* Curved and elevated text */}
         <text className="neon-text">
@@ -82,29 +82,30 @@ const HeartNeon: React.FC<HeartNeonProps> = ({ onActivated, onIgnite }) => {
           </textPath>
         </text>
 
-        {/* Lower, sharper flames surrounding the text */}
+        {/* Lower flames wrapping gracefully around and under the text */}
         <path
           className="flame"
-          d="M 160,530
-             Q 180,460 190,460 Q 200,500 220,510
-             Q 240,440 250,440 Q 260,490 280,510
-             Q 300,450 310,450 Q 320,500 340,520
-             Q 360,430 370,430 Q 380,480 400,500
-             Q 420,440 430,440 Q 440,490 460,510
-             Q 480,450 490,450 Q 500,500 520,520
-             Q 540,460 550,460 Q 560,500 580,510
-             Q 600,450 610,450 Q 620,520 640,540"
+          d="M 120,580 
+             C 150,480 180,480 200,560 
+             C 230,460 280,460 310,550 
+             C 340,470 390,470 420,560 
+             C 450,460 500,460 530,550 
+             C 560,480 600,480 620,560 
+             C 640,500 670,500 690,580"
         />
 
         <path
           className="flame"
-          d="M 140,580
-             Q 170,520 180,520 Q 210,560 230,570
-             Q 260,510 270,510 Q 290,550 310,570
-             Q 340,500 350,500 Q 380,550 410,570
-             Q 440,510 450,510 Q 480,560 510,570
-             Q 540,500 550,500 Q 580,560 600,580
-             Q 630,520 640,520 Q 660,570 670,600"
+          d="M 140,630 
+             C 180,550 220,550 250,620 
+             C 290,560 340,560 380,630 
+             C 420,560 470,560 510,630 
+             C 550,570 600,570 630,640"
+        />
+        
+        <path
+          className="flame"
+          d="M 200,670 Q 280,600 350,660 T 500,620 T 600,680"
         />
       </svg>
 
